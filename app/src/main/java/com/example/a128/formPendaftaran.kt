b916a8c5583840cb4906663901cfeda774a3d9a6
sekarasriii <sekareeesh@gmail.com>
2025-10-31 22:09:26 +0700

    membuat column utama formulir

--- app/src/main/java/com/example/a128/formPendaftaran.kt
+++ app/src/main/java/com/example/a128/formPendaftaran.kt
@@ -1,18 +1,25 @@
 package com.example.a128
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.graphics.Brush
 
 @Composable
 fun FormulirPendaftaran() {
@@ -52,5 +59,13 @@ fun FormulirPendaftaran() {
                 )
             )
             .padding(dimensionResource(id = R.dimen.padding_small))
-    )
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+        )
+    }
 }
