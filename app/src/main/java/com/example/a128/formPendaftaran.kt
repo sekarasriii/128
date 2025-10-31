ccaaeeb6c25afff559014ae0579a083e95a78b68
sekarasriii <sekareeesh@gmail.com>
2025-10-31 21:33:14 +0700

    menambahkan variabel untuk input pengguna

--- app/src/main/java/com/example/a128/formPendaftaran.kt
+++ app/src/main/java/com/example/a128/formPendaftaran.kt
@@ -1,6 +1,16 @@
 package com.example.a128
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
-fun FormulirPendaftaran() {}
+fun FormulirPendaftaran() {
+    //variabel untuk input pengguna
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember {mutableStateOf("")}
+    var textJK by remember {mutableStateOf("")}
+    var textStatus by remember {mutableStateOf("")}
+}
